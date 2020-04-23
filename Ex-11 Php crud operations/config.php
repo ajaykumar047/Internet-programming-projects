fad57731aeb5430c0bf7e171f62d2b8be586d41d
Ajaykumar U <51698008+ajaykumar047@users.noreply.github.com>
2020-04-23 17:51:56 +0530

    Update config.php

--- Ex-11 Php crud operations/config.php
+++ Ex-11 Php crud operations/config.php
@@ -8,6 +8,6 @@ $databaseName = 'crud_db';
 $databaseUsername = 'root';
 $databasePassword = '';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName,"3308"); 
  
 ?>
